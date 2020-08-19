c2f8f052a1ebdda602425c68c18698cdaa53d0d2
atraboulsi27 <a.traboulsi1@gmail.com>
2020-08-19 22:26:05 +0300

    trabelse

--- lib/Login/Register.dart
+++ lib/Login/Register.dart
@@ -1,19 +1,178 @@
 import 'package:flutter/material.dart';
-
+import 'package:hexcolor/hexcolor.dart';
 class Register extends StatefulWidget{
   @override
   _Register createState() => _Register();
 }
 
 class _Register extends State<Register>{
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passController = new TextEditingController();
+  TextEditingController cpassController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
+        width: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/books_wallpaper.png"),
+                fit: BoxFit.cover)),
         child: Column(
-          
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 5,
+            ),
+            Center(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Text(
+                        "Email",
+                        style: TextStyle(
+                            fontFamily: 'selawk',
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    Opacity(
+                      opacity: 0.8,
+                      child: Container(
+                        height: 52.0,
+                        width: 301,
+                        child: TextFormField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Hexcolor("#FFFFFF"),
+                            border: OutlineInputBorder(
+                                borderRadius: const BorderRadius.all(
+                                  const Radius.circular(0),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Hexcolor("#707070"),
+                                )),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+              child: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        child: Text(
+                          "Password",
+                          style: TextStyle(
+                              fontFamily: 'selawk',
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic),
+                        ),
+                      ),
+                      Opacity(
+                        opacity: 0.8,
+                        child: Container(
+                          height: 52.0,
+                          width: 301,
+                          child: TextFormField(
+                            controller: passController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Hexcolor("#FFFFFF"),
+                              border: OutlineInputBorder(
+                                  borderRadius: const BorderRadius.all(
+                                    const Radius.circular(0),
+                                  ),
+                                  borderSide: BorderSide(
+                                    color: Hexcolor("#707070"),
+                                  )),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+              child: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        child: Text(
+                          "Confirm Password",
+                          style: TextStyle(
+                              fontFamily: 'selawk',
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic),
+                        ),
+                      ),
+                      Opacity(
+                        opacity: 0.8,
+                        child: Container(
+                          height: 52.0,
+                          width: 301,
+                          child: TextFormField(
+                            controller: cpassController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Hexcolor("#FFFFFF"),
+                              border: OutlineInputBorder(
+                                  borderRadius: const BorderRadius.all(
+                                    const Radius.circular(0),
+                                  ),
+                                  borderSide: BorderSide(
+                                    color: Hexcolor("#707070"),
+                                  )),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 33, 0, 0),
+              child: Center(
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.0),
+                      side: BorderSide(color: Hexcolor("#707070"))),
+                  padding: EdgeInsets.symmetric(vertical: 9, horizontal: 29),
+                  color: Hexcolor("#E6D3D3").withOpacity(0.54),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => Register()));
+                  },
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(
+                      fontFamily: 'selawk',
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
